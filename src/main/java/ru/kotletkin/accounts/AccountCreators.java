7705c7df872ffd5d7ecd17bd60c30d3dd0fd198f
Vladislav Kotletkin <drzhkn1@mail.ru>
2024-12-18 23:14:10 +0300

    fix: Рефактор createChatMessageSender

--- src/main/java/ru/kotletkin/accounts/AccountCreators.java
+++ src/main/java/ru/kotletkin/accounts/AccountCreators.java
@@ -5,15 +5,13 @@ import org.telegram.telegrambots.meta.api.objects.message.Message;
 public class AccountCreators {
 
   public static ChatMessageSenderAccount createChatMessageSender(Message message) {
-    long id = message.getFrom().getId();
-    String firstNameUser = message.getFrom().getFirstName();
-    String lastNameUser = message.getFrom().getLastName();
-    String userNameUser = message.getFrom().getUserName();
-    boolean botStatus = message.getFrom().getIsBot();
-    long chatID = message.getChatId();
-    String messageText = message.getText();
-
     return new ChatMessageSenderAccount(
-        userNameUser, firstNameUser, lastNameUser, id, botStatus, chatID, messageText);
+        message.getFrom().getUserName(),
+        message.getFrom().getFirstName(),
+        message.getFrom().getLastName(),
+        message.getFrom().getId(),
+        message.getFrom().getIsBot(),
+        message.getChatId(),
+        message.getText());
   }
 }
